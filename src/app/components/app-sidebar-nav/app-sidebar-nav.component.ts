ba3c29cea269af3af447e3b20a6a103e0a34850f
Ahead <flashup4all@gmail.com>
2018-03-15 04:00:56 +0100

    updated membera image

--- src/app/components/app-sidebar-nav/app-sidebar-nav.component.ts
+++ src/app/components/app-sidebar-nav/app-sidebar-nav.component.ts
@@ -6,8 +6,8 @@ import { navigation } from './../../_nav';
 @Component({
   selector: 'app-sidebar-nav',
   template: `
-    <nav class="sidebar-nav">
-      <ul class="nav">
+    <nav class="sidebar-nav" style="width:250px !important;">
+      <ul class="nav" style="width:250px !important;">
         <ng-template ngFor let-navitem [ngForOf]="navigation">
           <li *ngIf="isDivider(navitem)" class="nav-divider"></li>
           <ng-template [ngIf]="isTitle(navitem)">
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application_name: 'Asusu - Loan Now Pay Later',
   api: {
   	version: '1.2',
-  	url: 'http://aix.testapi.asusu.ng/api/',//'http://asusuapi.asusu.ng/api/',
+  	url: 'http://ec2-13-59-107-189.us-east-2.compute.amazonaws.com/api/',//'http://aix.testapi.asusu.ng/api/',//'http://asusuapi.asusu.ng/api/',
   	imageUrl: 'http://aix.testapi.asusu.ng/images/',
   	payment_gateway_live: 'https://live.moneywaveapi.co/',
   	payment_gateway_test: 'https://moneywave.herokuapp.com/'
