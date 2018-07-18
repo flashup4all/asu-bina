220b9b4683517e73b4431fd486b32209daa76df1
Ahead <flashup4all@gmail.com>
2018-07-18 11:20:17 +0100

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   api: {
   	version: '1.2',
   	url: 'http://asusuapi.asusu.ng/api/',//'http://ec2-13-59-107-189.us-east-2.compute.amazonaws.com/api/',//'http://aix.testapi.asusu.ng/api/',//
-  	imageUrl: 'http://aix.testapi.asusu.ng/images/',
+  	imageUrl: 'http://asusuapi.asusu.ng/images/',//'http://aix.testapi.asusu.ng/images/',
   	payment_gateway_live: 'https://live.moneywaveapi.co/',
   	payment_gateway_test: 'https://moneywave.herokuapp.com/',
   	payment_key: {
